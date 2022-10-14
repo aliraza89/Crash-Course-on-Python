e7128ff905f09064b6ab8f11e35cec7f8d3d9023
aliraza89 <95914240+aliraza89@users.noreply.github.com>
2022-10-14 14:50:21 +0500

    Introduction to Python.py

---
+++ Introduction to Python.py
@@ -0,0 +1,27 @@
+#Fill in the correct Python command to put “My first Python program” onto the screen.
+>>> print("My first Python program")
+My first Python program
+
+#Convert this Bash command into Python:
+echo Have a nice day
+================================
+>>> print("Have a nice day")
+Have a nice day
+
+#Fill in the correct Python commands to put “This is fun!” onto the screen 5 times. 
+>>> for i in range(5):
+...     print("This is fun!")
+...
+This is fun!
+This is fun!
+This is fun!
+This is fun!
+This is fun!
+
+#Select the Python code snippet that corresponds to the following Javascript snippet:
+for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+===============================
+for i in range(10):
+  print(i)
